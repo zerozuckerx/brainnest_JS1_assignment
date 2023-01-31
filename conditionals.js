515bc802e3e95d694026469b55b16e455b1d5d18
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2023-01-31 20:44:45 +0100

    conditionals.js done

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+//display the higher number
+function higherNumber(num1,num2) {
+    if(num1 > num2) {
+        console.log(num1 + " is bigger than " + num2)
+    } else {
+        console.log(num2 + " is bigger than " + num1)
+    }
+}
+
+//display if a number is odd or even
+function evenOdd(number) {
+    if(number % 2 == 0) {
+        console.log("The number " + number + " is even.")
+    } else {
+        console.log("The number " + number + " is odd.")
+    }
+}
